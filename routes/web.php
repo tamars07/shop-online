83d422692b14353ca585fbc4a7b0a42800a2b5ae
RobertPhi <tranquocphipq@gmail.com>
2020-12-12 13:51:53 +0700

    add new branch Phi

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/cart', function () {
 Route::get('/products/{id_type}', 'ProductController@getProducts');
 
 Route::get('/product/{id_product}', 'ProductController@getProductDetail');
-
+//Đã thay đổi
 
 
 
