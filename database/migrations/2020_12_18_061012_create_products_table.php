2627e55f106a5684152520be3e07c81f102fbd3f
Trần Tâm <tamars07@yahoo.com>
2020-12-24 12:40:31 +0700

    migration files

---
+++ database/migrations/2020_12_18_061012_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255)->comment('day la ten sp');
+            $table->integer('id_type');
+            $table->integer('unit_price');
+            $table->integer('promotion_price')->nullable();
+            $table->text('image')->nullable();
+            $table->string('unit',50);
+            $table->boolean('new')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
