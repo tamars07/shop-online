2b0a013ba5ee5b894159d7635ea59a5dc714dc44
Trần Tâm <tamars07@yahoo.com>
2020-12-25 13:16:10 +0700

    admin master layouts

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    public function index(){
+    	return view('admin.index');
+    	//return view('layouts.admin.master');
+    }
+}
